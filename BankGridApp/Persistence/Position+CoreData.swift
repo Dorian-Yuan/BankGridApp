b706113c112d6927038d6f9e2bd4bef66a57f36a
Dorian <doriany@163.com>
2026-04-26 12:58:28 +0800

    fix: change Core Data codeGenerationType from Manual to class to fix momc crash

--- BankGridApp/Persistence/Position+CoreData.swift
+++ BankGridApp/Persistence/Position+CoreData.swift
@@ -1,24 +1,6 @@
 import Foundation
 import CoreData
 
-@objc(Position)
-public class Position: NSManagedObject {
-}
-
-extension Position {
-    @nonobjc class func fetchRequest() -> NSFetchRequest<Position> {
-        return NSFetchRequest<Position>(entityName: "Position")
-    }
-
-    @NSManaged public var code: String?
-    @NSManaged public var name: String?
-    @NSManaged public var short: String?
-    @NSManaged public var shares: Int32
-    @NSManaged public var basePrice: Double
-    @NSManaged public var avgCost: Double
-    @NSManaged public var totalCost: Double
-}
-
 extension Position {
     convenience init(context: NSManagedObjectContext, code: String, name: String, short: String, shares: Int32, basePrice: Double, avgCost: Double, totalCost: Double) {
         self.init(context: context)
